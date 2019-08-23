504b5c752ddee47c6372b2c8e9a306ff9918fb62
callum-marshall <callum_marshall@hotmail.co.uk>
2019-08-23 10:00:02 +0100

    add account.deposit

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -10,4 +10,9 @@ describe('Account', () => {
   test('account has a default balance', () => {
     expect(account.balance).toBe(0)
   })
+
+  test('user can make a deposit', () => {
+    account.deposit(200)
+    expect(account.balance).toBe(200)
+  })
 })
--- src/account.js
+++ src/account.js
@@ -2,6 +2,10 @@ class Account {
   constructor () {
     this.balance = 0
   }
+
+  deposit (value) {
+    this.balance += value
+  }
 }
 
 module.exports = {
